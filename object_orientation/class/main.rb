4addd6ebcfe4d41d5f6b52afa0610131b5a48ffb
caua-3301 <cauapires849@gmail.com>
2024-08-25 19:57:37 -0300

    object orientation

---
+++ object_orientation/class/main.rb
@@ -0,0 +1,21 @@
+#criando uma classe com ruby
+class Aluno
+
+  #permitindo leitura e acesso aos atributos (cria get e set de forma automática)
+  attr_accessor :nome, :telefone
+
+  def initialize (nome, telefone)
+    @nome = nome
+    @telefone = telefone
+  end
+
+  #getter
+  # def nome
+  #   @nome
+  # end
+
+  #setter
+  # def nome=(nome)
+  #   @nome = nome
+  # end
+end
